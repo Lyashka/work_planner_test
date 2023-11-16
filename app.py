7926fa8774b4d61f266c92b35d273e34fba716b2
Lyashka <marzlyaha@mail.ru>
2023-11-16 17:22:29 +0300

    table save an prev table

--- app.py
+++ app.py
@@ -3,15 +3,16 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Load data
 df = pd.read_excel('./data/planner.xlsx')
 
-# Dictionary to store changes
 changes = {}
+
+df.index = df.index + 1
 
 @app.route('/')
 def display_table():
-    return render_template('index.html', table=df.to_html(classes='data'), title='Таблица Excel')
+    
+    return render_template('index.html', table=df.to_html(classes='data'), title='Главная таблица')
 
 @app.route('/edit', methods=['POST'])
 def edit():
@@ -27,16 +28,21 @@ def edit():
 
 @app.route('/save_changes', methods=['POST'])
 def save_changes():
-    # new_value = {}
-    new_value=request.json
-    
-    print(new_value)
-    print('________________')
     global df
-    # Apply changes to the DataFrame
-    for (row, col), value in changes.items():
-        df.iat[row, col-1] = value
+    # сохраним df в качестве предыдущей версии
+    excel_file_path = './data/prev_planner.xlsx'
+    df.to_excel(excel_file_path, index=False)
 
+    new_value=request.json
+    changes = new_value['arr']
+
+    print('________________')
+   
+    # Apply changes to the DataFrame
+    for  value in changes:
+        df.iat[value['row'], value['col']-1] = value['newContent']
+        
+    print(df)
     # Save changes to the Excel file
     df.to_excel('./data/planner.xlsx', index=False)
     
@@ -44,6 +50,29 @@ def save_changes():
     changes.clear()
 
     return jsonify({'status': 'success'})
+
+
+@app.route('/prev_planner', methods=['GET'])
+def prev_planner():
+    global prev_df
+    prev_df = pd.read_excel('./data/prev_planner.xlsx')
+    print(prev_df)
+    print(df)
+    prev_df.index = prev_df.index + 1
+    return render_template('index.html', table=prev_df.to_html(classes='data'), title='Предыдущая таблица')
+
+
+@app.route('/rollback_planner', methods=['POST'])
+def rollback_planner(): 
+    global prev_df
+    global df
+    df = prev_df
+    df = pd.read_excel('./data/prev_planner.xlsx')
+
+    excel_file_path = './data/planner.xlsx'
+    df.to_excel(excel_file_path, index=False)
+    return render_template('index.html', table=df.to_html(classes='data'), title='Предыдущая таблица')
+
 
 if __name__ == '__main__':
     app.run()
